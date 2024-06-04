a2a27b3861c322e9cd513b30549f9f9815a6a0db
iShoockx <160795305+iShoockx@users.noreply.github.com>
2024-06-04 13:22:08 -0300

    A

--- dash.py
+++ dash.py
@@ -42,9 +42,8 @@ with st.sidebar:
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 
 #Filtragem de dados pos select box
-crit=int(df_selected_series["critics score"].dropna(how=all))
-aud=int(df_selected_series["audience score"].dropna(how=all))
-Delta_semana=(df_marvel_full_selected['1st vs 2nd weekend drop off'].dropna(how=all))
+crit=int(df_selected_series["critics score"].iloc[0])
+aud=int(df_selected_series["audience score"].iloc[0])
 
 
 
@@ -54,16 +53,16 @@ Delta_semana=(df_marvel_full_selected['1st vs 2nd weekend drop off'].dropna(how=
 # a função deve retornar o objeto do gráfico.
 # Use e abuse do Plotly!
 
-def Format_Milion(num):
+def Format_Milion(num):# Funçao para formatar valor para milhao
     return f'{num} M'
 
-def graph_bar(df_input,input_x,input_y,input_color):
-    graph_comparaçao = px.bar(df_input, x=input_x, y=input_y, color= input_color ,barmode='group' )
+def graph_bar(df_input,input_x,input_y,input_color):# Grafico de Barra
+    graph_comparaçao = px.bar(df_input, x=input_x, y=input_y, color= input_color ,barmode='group',height=350 )
     return graph_comparaçao 
 
-
-def make_donut(input_response, input_text, input_color):
+def make_donut(input_response, input_text, input_color):#Grafico de donut
   int(input_response)
+  # Escolha de cores
   if input_color == 'blue':
       chart_color = ['#29b5e8', '#155F7A']
   if input_color == 'green':
@@ -72,7 +71,7 @@ def make_donut(input_response, input_text, input_color):
       chart_color = ['#F39C12', '#875A12']
   if input_color == 'red':
       chart_color = ['#E74C3C', '#781F16']
-    
+    # Criaçao da lista utilizada para o grafico
   source = pd.DataFrame({
       "Topic": ['', input_text],
       "% value": [100-input_response, input_response]
@@ -82,7 +81,7 @@ def make_donut(input_response, input_text, input_color):
       "% value": [100, 0]
   })
     
-  plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
+  plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode( #Criação do Grafico de donut
       theta="% value",
       color= alt.Color("Topic:N",
                       scale=alt.Scale(
@@ -92,7 +91,7 @@ def make_donut(input_response, input_text, input_color):
                           range=chart_color),
                       legend=None),
   ).properties(width=130, height=130)
-    
+    # Formataçao do Texto do Grafico
   text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
   plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=45, cornerRadius=20).encode(
       theta="% value",
@@ -111,37 +110,39 @@ def make_donut(input_response, input_text, input_color):
 # principal do dashboard 
 
 # criando colunas e suas larguras
-col = st.columns((3, 2.5 ,3), gap='large')
-
+col = st.columns((2.5, 2.5 ,3), gap='large')
+# Primeira coluna
 with col[0]:
-    
-    # Metric
+
+    # Metricas
     st.markdown("#### 1ª vs 2ª Semana")
     st.metric(label="Semana de abertura", value=Format_Milion(df_marvel_full_selected['opening weekend'].iloc[0]))
-    
     st.metric(label="Segunda Semana", value=Format_Milion(df_marvel_full_selected["second weekend"].iloc[0]), 
-              delta=df_marvel_full_selected['1st vs 2nd weekend drop off'].iloc[0])
+        delta=df_marvel_full_selected['1st vs 2nd weekend drop off'].iloc[0])
     
     # Grafico de Barras
     st.markdown('#### Lucro/Custo')
-    p1 = graph_bar(df_selected_series,"Filmes","Valor em (m$)","Lucro/Custo")
-    st.plotly_chart(p1, use_container_width=True)
+    Graph = graph_bar(df_selected_series,"Filmes","Valor em (m$)","Lucro/Custo")# Chamada do plot de Grafico de Barra
+    st.plotly_chart(Graph, use_container_width=True)
+
+# Segunda Coluna 
 
 with col[1]:
-
-    # Imagem Banner
     st.markdown("#### Filme")
-    st.image(df_selected_series['Imagens'].values[0], use_column_width=True, output_format='JPEG',)
-    
+    cols2 = st.columns((1,8,1))
+    with cols2[1]:
+        # Imagem Banner
+        st.image(df_selected_series['Imagens'].values[0], width=200, output_format='JPEG')
+
     # Grafico de donut
     st.markdown('#### Notas')
     aud = make_donut(aud,"críticos","blue")
     crit = make_donut(crit,"audiência","orange")
-    notas_col = st.columns((5, 5))
-    with notas_col[0]:
+    cols1 = st.columns((5, 5))
+    with cols1[0]:
         st.write('Audiência')
         st.altair_chart(aud)
-    with notas_col[1]:   
+    with cols1[1]:   
         st.write('Críticos')
         st.altair_chart(crit)
 
@@ -159,7 +160,7 @@ with col[2]:
     # Sobre
     with st.expander('Sobre', expanded=True):
         st.write('''
-            - Data: [MARVEL Movies - Box Office Data]( https://www.kaggle.com/datasets/jainaru/marvel-movies-box-office-data).
+            - Data: [MARVEL Movies - Box Office Data](https://github.com/iShoockx/Dashboard_python/tree/main/Data).
             - :orange[**Lucro/Custo**]: O grafico Mostra o Valor que foi gasto para realizar o filme e quando arrecadou de volta .
             - :orange[**Notas**]: A porcentagem em media da nota da audiencia e dos criticos .
             - :orange[**1ª vs 2ª Semana**]: O delta nesse topico representa a porcentagem de ganho que foi perdido entre a primeira e a segunda semana.
